1795316f8cf362624aeb3a4cd03caef2bc4f789f
Eric Negreido <ericnegreidooo@gmail.com>
2024-04-24 08:24:16 -0300

    pagina inicial con un sencillo formulario

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="authentication.php" method="post">
+
+  <table>
+    <tr>
+      <td class="izq">
+        USER:
+      </td>
+      <td class="der">
+        <input type="text" name="login"></td></tr>
+        <tr><td class="izq">
+        PASSWORD:
+      </td>
+      <td class="der">
+        <input type="password" name="password"></td></tr>
+        <tr><td colspan="2"> <input type="submit" name="send" value="login"></td></tr></table>
+  </table>
+  </form>
+</body>
+</html>
