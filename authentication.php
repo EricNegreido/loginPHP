ea10e4320fe615b905bd8e5af11993e88feb811d
Eric Negreido <ericnegreidooo@gmail.com>
2024-04-26 19:09:46 -0300

    Primera parte login con conexion PDO

--- authentication.php
+++ authentication.php
@@ -3,8 +3,8 @@
 try{
 
   $base = new PDO("mysql:host=localhost; dbname=login", "root","");
-  $base->setAttribute(PDO::ATTR_MODE, PDO::ERRMODE_EXCEPTION);
-  $sql ="SELECT * FROM users_pass WHERE aUSER= :login AND PASSWORD = :password";
+  $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql ="SELECT * FROM users_pass WHERE USER= :login AND PASSWORD = :password";
   $resultado = $base->prepare($sql);
   $login = htmlentities(addslashes($_POST["login"]));
   $password = htmlentities(addslashes($_POST["password"]));
@@ -15,9 +15,10 @@ try{
   $number_reg = $resultado->rowCount();
 
   if($number_reg){
-    
+    echo "Se Inicio Sesion";
   }else{
-
+    //Redirigir a login nueveamente
+    header("location:index.php");
   }
 }catch(Exception $e){
   die("ERROR: " . $e->getMessage());
